9fc3b30016eef3d5da2158e6c5178fb0ace85329
0xDev05 <divpatel1274@gmail.com>
2025-08-27 18:56:20 +0530

    take functionality implemented

--- src/instructions/take.rs
+++ src/instructions/take.rs
@@ -1,1 +1,94 @@
+use pinocchio::{account_info::AccountInfo, program_error::ProgramError, ProgramResult};
 
+use crate::{
+    take, AccountCheck, AssociatedTokenAccount, AssociatedTokenAccountCheck,
+    AssociatedTokenAccountInit, MintInterface, ProgramAccount, SignerAccount,
+};
+
+pub struct TakeAccounts<'a> {
+    pub taker: &'a AccountInfo,
+    pub maker: &'a AccountInfo,
+    pub escrow: &'a AccountInfo,
+    pub mint_a: &'a AccountInfo,
+    pub mint_b: &'a AccountInfo,
+    pub vault: &'a AccountInfo,
+    pub taker_ata_a: &'a AccountInfo,
+    pub taker_ata_b: &'a AccountInfo,
+    pub maker_ata_b: &'a AccountInfo,
+    pub system_program: &'a AccountInfo,
+    pub token_program: &'a AccountInfo,
+}
+
+impl<'a> TryFrom<&'a [AccountInfo]> for TakeAccounts<'a> {
+    type Error = ProgramError;
+    fn try_from(accounts: &'a [AccountInfo]) -> Result<Self, Self::Error> {
+        let [taker, maker, escrow, mint_a, mint_b, vault, taker_ata_a, taker_ata_b, maker_ata_b, system_program, token_program, ..] =
+            accounts
+        else {
+            return Err(ProgramError::NotEnoughAccountKeys);
+        };
+
+        //basic accounts check
+        SignerAccount::check(taker)?;
+        ProgramAccount::check(escrow)?;
+        MintInterface::check(mint_a)?;
+        MintInterface::check(mint_b)?;
+        AssociatedTokenAccount::check(taker_ata_b, taker, mint_b, token_program)?;
+        AssociatedTokenAccount::check(vault, escrow, mint_a, token_program)?;
+
+        Ok(Self {
+            taker,
+            maker,
+            escrow,
+            mint_a,
+            mint_b,
+            vault,
+            taker_ata_a,
+            taker_ata_b,
+            maker_ata_b,
+            system_program,
+            token_program,
+        })
+    }
+}
+
+pub struct Take<'a> {
+    pub accounts: TakeAccounts<'a>,
+}
+
+impl<'a> TryFrom<&'a [AccountInfo]> for Take<'a> {
+    type Error = ProgramError;
+
+    fn try_from(accounts: &'a [AccountInfo]) -> Result<Self, Self::Error> {
+        let accounts = TakeAccounts::try_from(accounts)?;
+
+        AssociatedTokenAccount::init_if_needed(
+            accounts.maker_ata_b,
+            accounts.mint_b,
+            accounts.taker,
+            accounts.maker,
+            accounts.system_program,
+            accounts.token_program,
+        )?;
+
+        AssociatedTokenAccount::init_if_needed(
+            accounts.taker_ata_a,
+            accounts.mint_a,
+            accounts.taker,
+            accounts.taker,
+            accounts.system_program,
+            accounts.token_program,
+        )?;
+
+        Ok(Self { accounts })
+    }
+}
+
+
+impl<'a> Take<'a> {
+    pub const DISCRIMINATOR:&'a u8 = &1;
+
+    pub fn process(&mut self) -> ProgramResult{
+        
+    }
+}
